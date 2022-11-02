6e7345c659985f51286ae86ef07285e9522796b0
jesus Manuel Tortolero <xjtormar883@ieshnosmachado.org>
2022-11-02 12:24:54 +0100

    primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,38 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+
+export class HeroeComponent{
+    nombre: string = 'Ironman';
+    edad: number = 45;
+
+    get nombreCapitalizado(): string{
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string{
+        return `${this.nombre} - ${this.edad}`
+    }
+
+    cambiarNombre():void{
+        if(this.nombre == 'Ironman'){
+            this.nombre = 'Spiderman';
+        }
+        else{
+            this.nombre = 'Ironman';
+        }
+    }
+
+    cambiarEdad():void{
+        if(this.edad == 45){
+            this.edad = 30;
+        }
+        else{
+            this.edad = 45;
+        }
+    }
+}
